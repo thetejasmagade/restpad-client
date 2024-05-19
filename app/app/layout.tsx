da4020710715075f31e7b6f77ee55638c8abf3b6
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2024-05-19 20:16:17 +0530

    remove debug

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -41,25 +41,6 @@ export default function RootLayout({
     else setLoading(false);
   };
 
-  const { data } = supabase.auth.onAuthStateChange((event, session) => {
-    console.log(event, session);
-    console.log(data);
-  
-    if (event === 'INITIAL_SESSION') {
-      // handle initial session
-    } else if (event === 'SIGNED_IN') {
-      // handle sign in event
-    } else if (event === 'SIGNED_OUT') {
-      // handle sign out event
-    } else if (event === 'PASSWORD_RECOVERY') {
-      // handle password recovery event
-    } else if (event === 'TOKEN_REFRESHED') {
-      // handle token refreshed event
-    } else if (event === 'USER_UPDATED') {
-      // handle user updated event
-    }
-  })
-
   return (
     <>
       {loading ? (
